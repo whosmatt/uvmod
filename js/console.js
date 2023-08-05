4ba14f36fb7ea9a1046300ed1c296d0f847d9d58
matt <10769515+whosmatt@users.noreply.github.com>
2023-08-05 19:55:21 +0200

    moved log function

--- js/console.js
+++
@@ -1,9 +0,0 @@
-function log(message) {
-    const consoleArea = document.getElementById('console');
-
-    // Append the new message to the existing content and add a newline
-    consoleArea.value += message + '\n';
-
-    // Scroll to the bottom to show the latest message
-    consoleArea.scrollTop = consoleArea.scrollHeight;
-}
--- js/modframework.js
+++ js/modframework.js
@@ -118,6 +118,16 @@ function applyMods(firmware) {
   return firmware;
 }
 
+function log(message) {
+  const consoleArea = document.getElementById('console');
+
+  // Append the new message to the existing content and add a newline
+  consoleArea.value += message + '\n';
+
+  // Scroll to the bottom to show the latest message
+  consoleArea.scrollTop = consoleArea.scrollHeight;
+}
+
 
 
 // Helper functions:
